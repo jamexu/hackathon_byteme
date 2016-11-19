f72e7686ff6f5dd7ea5d27a052cf5db0d1940f51
James Xu <xujamesx@sfu.ca>
2016-11-19 21:55:14 +0000

    new ui design, with added features such as swiping toast

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var globalToastCounter = 0;
+var left;
+var isLeft;
+$( document ).ready(function() {
+    Materialize.toast("First toast",999999999,'', function(){newToast();});
+});
+
+ 
+globalToastCounter++;
+$( "#toast-container" ).mousedown(function() {
+    left = $(".toast").offset().left;
+    console.log("mouse down " + left);
+});
+
+$( "#toast-container" ).mouseup(function() {
+    var newleft = $(".toast").offset().left;
+    console.log("mouse up " + newleft);
+    if(left < newleft){
+        isLeft = false;
+    }
+    else{
+        isLeft = true;
+    }
+});
+
+function newToast(){
+    //var left = $(this).offset().left;
+    console.log("toast dismissed " + isLeft);
+    if(isLeft){
+        Materialize.toast("I AM NEW TOAST, you swiped left",999999999,'', function(){
+            newToast();
+        });
+    }
+    else{
+        Materialize.toast("I AM NEW TOAST, you swiped right",999999999,'', function(){
+            newToast();
+        });
+    }
+
+}
+
+// setTimeout(function(){
+//     console.log("in timer");
+//     if(globalToastCounter <10){
+//         console.log("toast number: " + globalToastCounter);
+//         globalToastCounter ++;
+//         newToast("new toast" + globalToastCounter);
+//         console.log("toasting");
+//     }
+// }, 5000);
