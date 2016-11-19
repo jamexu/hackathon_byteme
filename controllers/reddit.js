1991a560485bbb1972c1109df96dd80472d82964
Jay Svoboda <jay.svoboda@outlook.com>
2016-11-19 14:24:54 -0800

    fix indent

--- controllers/reddit.js
+++ controllers/reddit.js
@@ -5,32 +5,30 @@ reddit.getArticles = function (req, res) {
 	title = "WELCOME TO /r/" + req.params.subreddit
 
 	parser.parseURL('https://www.reddit.com/r/' + req.params.subreddit + '.rss', function(err, parsed) {
-		  largeString = ''
-			count = 0
+		largeString = '';
+		count = 0;
 
-			parsed.feed.entries.forEach(function(entry) {
-				  count += 1
-					var entryTitle = entry.title
-					var url = entry.link
-					largeString += '<a href="' + url + '">' + entryTitle + '</a><br><br>'
+		parsed.feed.entries.forEach(function(entry) {
+			count += 1;
+			var entryTitle = entry.title;
+			var url = entry.link;
+			largeString += '<a href="' + url + '">' + entryTitle + '</a><br><br>';
 
-					var gifv_pattern = /http:\/\/i\.imgur\.com\/(\w|_|-)+\.gifv/g;
-					if (gifv_pattern.test(entry.content)) {
-							var gifv = entry.content.match(gifv_pattern)
-							console.log('gifv ' + gifv)
-							largeString += '<video preload="auto" autoplay="autoplay" loop="loop" style="width: 200px; height: 200px;"><source src="' + gifv.toString().replace('.gifv', '.mp4') + '" type="video/mp4"></source></video><br><br>'
-					}
-					else {
-							var thumbnail_pattern = /https:\/\/[a-z]\.thumbs\.redditmedia\.com\/(\w|_|-)+\.jpg/g;
-							if (thumbnail_pattern.test(entry.content)) {
-									var thumbnail = entry.content.match(thumbnail_pattern)
-									largeString += '<img src="' + thumbnail + '" alt="' + entryTitle + '"><br><br>'
-							}
-					}
-			})
-			res.send(title + '<br>Parsed ' + count + ' images. <br><br>' + largeString)
-	})
-
+			var gifv_pattern = /http:\/\/i\.imgur\.com\/(\w|_|-)+\.gifv/g;
+			if (gifv_pattern.test(entry.content)) {
+				var gifv = entry.content.match(gifv_pattern);
+				console.log('gifv ' + gifv);
+				largeString += '<video preload="auto" autoplay="autoplay" loop="loop" style="width: 200px; height: 200px;"><source src="' + gifv.toString().replace('.gifv', '.mp4') + '" type="video/mp4"></source></video><br><br>';
+			} else {
+				var thumbnail_pattern = /https:\/\/[a-z]\.thumbs\.redditmedia\.com\/(\w|_|-)+\.jpg/g;
+				if (thumbnail_pattern.test(entry.content)) {
+					var thumbnail = entry.content.match(thumbnail_pattern);
+					largeString += '<img src="' + thumbnail + '" alt="' + entryTitle + '"><br><br>';
+				}
+			}
+		});
+		res.send(title + '<br>Parsed ' + count + ' images. <br><br>' + largeString);
+	});
 };
 
 reddit.scoreArticle = function (req, res) {
